29e4d692ee2a1617ea5d306ab62910a3695b2093
Som <46430603+Som501103@users.noreply.github.com>
2019-09-20 11:17:42 +0700

    script

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
+jQuery.ajax({
+  url: "http://127.0.0.1:8000/api/getpeaid/",
+  // url: "https://hookb.in/3OynwLEapdhKeKj2MjmJ",
+  type: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  dataType: 'json',
+  data: JSON.stringify({
+    'uid' : "U649ce5cbe448b470e9a8eb4557952a3b"
+    })
+  })
+  .done(function(data, textStatus, jqXHR) {
+      console.log("HTTP Request Succeeded: " + jqXHR.status);
+      console.log(data); //Return Data
+      if (jqXHR.status == 200) {
+
+          var staffname = data['obj']['FirstName'];
+          var stafflastname = data['obj']['LastName'];
+          var staffdept = data['obj']['DepartmentShort'];
+          var stafftel = data['obj']['DepartmentShort'];
+          var subregion = data['obj']['SubRegionCode'];
+
+          console.log(staffname);
+          document.getElementById('staffname').innerHTML = staffname + "  " + stafflastname;
+          document.getElementById('staffdept').innerHTML = staffdept;
+          document.getElementById('stafftel').innerHTML = stafftel;
+
+
+      };
+    })
+
+
 function validateForm() {
 
-  var groupchk = document.forms['myForm'].elements['group[]'];
-  var txt = "";
+  // var groupchk = document.forms['myForm'].elements['group[]'];
+  // var txt = "";
+  // var i;
+  // for (i = 0; i < groupchk.length; i++) {
+  //   if (groupchk[i].checked) {
+  //     txt = txt + groupchk[i].value;
+  //   }
+  // }
+
+
+  var maincausetype = document.forms['myForm'].elements['maincause'];
+  var maincause = "";
   var i;
-  for (i = 0; i < groupchk.length; i++) {
-    if (groupchk[i].checked) {
-      txt = txt + groupchk[i].value;
+  for (i = 0; i < maincausetype.length; i++) {
+    if (maincausetype[i].checked) {
+      maincause = maincause + maincausetype[i].value;
     }
   }
 
@@ -19,104 +62,62 @@ function validateForm() {
   }
 
 
-    var datehappen = document.forms["myForm"]["datehappen"].value;
-    var timehappen = document.forms["myForm"]["timehappen"].value;
-    var daterestore = document.forms["myForm"]["daterestore"].value;
-    var timerestore = document.forms["myForm"]["timerestore"].value;
-    var course = document.forms["myForm"]["course"].value;
-    var timeexpect = document.forms["myForm"]["timeexpect"].value;
-    var equipcode = document.forms["myForm"]["equipcode"].value;
-    // var triptype = document.forms["myForm"]["trip"].value;
-    var relay = document.forms["myForm"]["relay"].value;
-    var load = document.forms["myForm"]["load"].value;
-    var load_A = document.forms["myForm"]["load_A"].value;
-    var load_B = document.forms["myForm"]["load_B"].value;
-    var load_C = document.forms["myForm"]["load_C"].value;
-    var load_G = document.forms["myForm"]["load_G"].value;
-    var detail = document.forms["myForm"]["detail"].value;
-    var lon = document.getElementById("lon").value;
-    var lat = document.getElementById("lat").value;
-    // var filename = document.getElementById('fileButton').files[0].name;
 
-    // var img= document.forms["myForm"]["img"].value;
+  jQuery.ajax({
+    url: "http://127.0.0.1:8000/api/lineliff/",
+    // url: "https://hookb.in/3OynwLEapdhKeKj2MjmJ",
+    type: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    dataType: 'json',
+    data: JSON.stringify({
 
+      "datehappen" : document.forms["myForm"]["datehappen"].value,
+      "daterestore" : document.forms["myForm"]["daterestore"].value,
+      "course": document.forms["myForm"]["course"].value,
+      "equipcode": document.forms["myForm"]["equipcode"].value,
+      "peacode": document.forms["myForm"]["peacode"].value,
+      "fedder": document.forms["myForm"]["fedder"].value,
+      "fedderwork": document.forms["myForm"]["fedderwork"].value,
+      "relay": document.forms["myForm"]["relay"].value,
+      "load": document.forms["myForm"]["load"].value,
+      "equipstrat": document.forms["myForm"]["equipstrat"].value,
+      "equipend": document.forms["myForm"]["equipend"].value,
+      "bk": document.forms["myForm"]["bk"].value,
+      "r": document.forms["myForm"]["r"].value,
+      "load_A": document.forms["myForm"]["load_A"].value,
+      "load_B": document.forms["myForm"]["load_B"].value,
+      "load_C": document.forms["myForm"]["load_C"].value,
+      "load_G": document.forms["myForm"]["load_G"].value,
+      "distance": document.forms["myForm"]["distance"].value,
+      "maincause": maincause,
+      "weather": document.forms["myForm"]["weather"].value,
+      "area": document.forms["myForm"]["area"].value,
+      "trip": trip
 
-    // alert("complease...");
-    var myObj = { firstname : "John", lastname : "Doe" };
-    console.log(myObj);
-    console.log("datehappen");
-    console.log(datehappen);
-    console.log(document.getElementById("lon").value);
-    console.log(document.getElementById("lat").value);
-
-    // console.log("img");
-    // console.log(img);
-    // alert("Name must be filled out");
-
-    // var firebaseConfig = {
-    //     apiKey: "AIzaSyDbwz3Nhbc47UwxZoF9_2Sy3x1ouPmWmJE",
-    //     authDomain: "updatetable-ce5d8.firebaseapp.com",
-    //     databaseURL: "https://updatetable-ce5d8.firebaseio.com",
-    //     projectId: "updatetable-ce5d8",
-    //     storageBucket: "updatetable-ce5d8.appspot.com",
-    //     messagingSenderId: "1073280316561",
-    //     appId: "1:1073280316561:web:9cd3a219b71a0e29"
-    // };
-    // // Initialize Firebase
-    // firebase.initializeApp(firebaseConfig);
-
-
-  //   const firebaseConfig = {
-  //   apiKey: "AIzaSyCzCyaklYiapilTfCDP2GwrrMnao-9TETM",
-  //   authDomain: "rc2line-3b221.firebaseapp.com",
-  //   databaseURL: "https://rc2line-3b221.firebaseio.com",
-  //   projectId: "rc2line-3b221",
-  //   storageBucket: "rc2line-3b221.appspot.com",
-  //   messagingSenderId: "272565657956",
-  //   appId: "1:272565657956:web:2f2698bd1625c6ab"
-  // };
-  //   // Initialize Firebase
-  //   firebase.initializeApp(firebaseConfig);
-
-    // var fileButton = document.getElementById("fileButton");
-    //           fileButton.addEventListener('change', function(e){
-    //               var file = e.target.files[0];
-    //               var storageRef = firebase.storage().ref(file.name);
-    //               storageRef.put(file);
-    //           });
-
-    var db = firebase.firestore();
-    var timestamp = Date.now();
-
-    db.collection("test1").doc("rc"+timestamp).set({
-        datehappen1: datehappen,
-        timehappen1: timehappen,
-        daterestore1: daterestore,
-        timerestore1: timerestore,
-        course1: course,
-        timeexpect1: timeexpect,
-        equipcode1: equipcode,
-        triptype1: trip,
-        relay1: relay,
-        phase1: txt,
-        load1: load,
-        load_A1: load_A,
-        load_B1: load_B,
-        load_C1: load_C,
-        load_G1: load_G,
-        detail1: detail,
-        lat1: lat,
-        lon1: lon
-        // storageRef1: storageRef
-        // img1: img
     })
-        .then(function(docs) {
-            console.log(docs.data());
 
-        })
-        .catch(function(error) {
-            console.error("Error adding document: ", error);
-        });
+  })
+  .done(function(data, textStatus, jqXHR) {
+      console.log("HTTP Request Succeeded: " + jqXHR.status);
+      console.log(data); //Return Data
+      if (jqXHR.status == 200) {
+
+      //   // TODO: Check condition Comp_type
+      //   // if Comp_type == 1 || Comp_type == 2
+      //   //window.location = "p11searchp.html"
+      //   // else if (Comp_type == 3) {
+      //   //window.location = "p11searchp.html"
+      //   // }
+
+      //   //window.location = "p11searchp.html"
+
+       window.location.replace("file:///Users/som501103/Downloads/rc2-test2/login.html")
+
+      };
+    })
+
 
     return false;
 
@@ -141,3 +142,185 @@ function showMyImage(fileInput) {
             reader.readAsDataURL(file);
         }
     }
+
+    $(document).ready(function() {
+    // Initializing arrays with city names.
+    var TC001 = [{
+    display: "กิ่งไม้พลาดสาย",value: "CC01001"},
+    {display: "กิ่งไม้โตมาแตะสาย", value: "CC01002"},
+    {display: "ต้นไม้ล้มทับสาย",value: "CC01003"},
+    {display: "ต้นไม้เลื้อยมาแตะสาย",value: "CC01004"},
+    {display: "อื่นๆ",value: "CC01005"}];
+    var TC002 = [{
+    display: "อุปกรณ์ชำรุด",value: "CC02001"},
+    {display: "ทำงานผิดพลาด",value: "CC02002"},
+    {display: "ติดตั้งไม่ได้มาตรฐาน",value: "CC02003"},
+    {display: "อุปกรณ์ทำงานไม่สัมพันธ์กัน",value: "CC02004"},
+    {display: "อื่นๆ",value: "CC02005"}];
+    var TC003 = [{
+    display: "ผู้สั่งการ",value: "CC03001"},
+    {display: "พนักงานบำรุงรักษา",value: "CC03002"},
+    { display: "พนักงานก่อสร้าง",value: "CC03003"},
+    {display: "คนงาน", value: "CC03004"},
+    {display: "พนักงาน/ช่างบริษัทที่รับเหมากฟภ.", value: "CC03005"},
+    {display: "อื่นๆ", value: "CC03006"}];
+    var TC004 = [{
+    display: "คนตัดต้นไม้",value: "CC04001"},
+    {display: "คนงานพาดสายโทรศัพท์ู",value: "CC04002"},
+    { display: "คนยิงลูกถ้วย",value: "CC04003"},
+    {display: "คนลักไฟใช้", value: "CC04004"},
+    {display: "อื่นๆ", value: "CC04005"}];
+    var TC005 = [{
+    display: "นก/ค้างคาว",value: "CC05001"},
+    {display: "งู",value: "CC05002"},
+    { display: "แมลง/แมง",value: "CC05003"},
+    {display: "ลิง/ค่าง", value: "CC05004"},
+    {display: "แมว", value: "CC05005"},
+    {display: "กระรอก/กระแต/หนู", value: "CC05006"},
+    {display: "ตุ๊กแก", value: "CC05007"},
+    {display: "อื่นๆ", value: "CC05008"}];
+    var TC006 = [{
+    display: "รถยนต์",value: "CC06001"},
+    {display: "รถก่อสร้าง",value: "CC06002"},
+    {display: "รถบรรทุก",value: "CC06003"},
+    {display: "ยานพาหนะทางน้ำ", value: "CC06004"},
+    {display: "ยานพาหนะทางอากาศ", value: "CC06005"},
+    {display: "เครื่องจักรกล/ปั้นจั่น/เครน", value: "CC06006"},
+    {display: "อื่นๆ", value: "CC06007"}];
+    var TC007 = [{
+    display: "ว่าว",value: "CC07001"},
+    {display: "โคมลอย",value: "CC07002"},
+    { display: "ป้ายโฆษณา",value: "CC07003"},
+    {display: "ลูกโป่ง", value: "CC07004"},
+    {display: "สังกะสีและแผ่นโลหะ", value: "CC07005"},
+    {display: "เสาอากาศทีวีล้มทับนู", value: "CC07006"},
+    {display: "วัสดุก่อสร้างแตะสาย", value: "CC07007"},
+    {display: "ฟาง,ใบอ้อย,ทางมะพร้าวปลิว", value: "CC07008"},
+    {display: "อื่นๆ", value: "CC07009"}];
+    var TC008 = [{
+    display: "ไฟไหม้",value: "CC08001"},
+    {display: "ฝุ่น",value: "CC08002"},
+    {display: "ฟ้าผ่า",value: "CC08003"},
+    {display: "น้ำเซาะ", value: "CC08004"},
+    {display: "ดินทรุด", value: "CC08005"},
+    {display: "ไอเกลือ", value: "CC08006"},
+    {display: "ตะไคร่น้ำจับสาย", value: "CC08007"},
+    {display: "อื่นๆ", value: "CC08008"}];
+    var TC009 = [{
+    display: "พายุ/ดีเปรสชั่น/ใต้ฝุ่น",value: "CC09001"},
+    {display: "แผ่นดินไหว",value: "CC09002"},
+    { display: "น้ำท่วม (อุทกภัย)",value: "CC09003"},
+    {display: "ไฟป่า", value: "CC09004"},
+    {display: "โคลนถล่ม", value: "CC09005"},
+    {display: "ลูกเห็บู", value: "CC09006"},
+    {display: "อื่นๆ", value: "CC05007"}];
+    var TC010 = [{
+    display: "สงคราม/จราจล",value: "CC10001"}];
+    var TC011 = [{
+    display: "จ่ายเกินพิกัด",value: "CC11001"}];
+    var TC012 = [{
+    display: "อื่นๆ",value: "CC12001"}];
+    // Function executes on change of first select option field.
+    $("#country").change(function() {
+    var select = $("#country option:selected").val();
+    switch (select) {
+    case "TC001":
+    city(TC001);
+    break;
+    case "TC002":
+    city(TC002);
+    break;
+    case "TC003":
+    city(TC003);
+    break;
+    case "TC004":
+    city(TC004);
+    break;
+    case "TC005":
+    city(TC005);
+    break;
+    case "TC006":
+    city(TC006);
+    break;
+    case "TC007":
+    city(TC007);
+    break;
+    case "TC008":
+    city(TC008);
+    break;
+    case "TC009":
+    city(TC009);
+    break;
+    case "TC010":
+    city(TC010);
+    break;
+    case "TC011":
+    city(TC011);
+    break;
+    case "TC012":
+    city(TC012);
+    break;
+    default:
+    $("#city").empty();
+    $("#city").append("<option>กรุณาเลือกสาเหตุ</option>");
+    break;
+    }
+    });
+    // Function To List out Cities in Second Select tags
+    function city(arr) {
+    $("#city").empty(); //To reset cities
+    $("#city").append("<option>กรุณาเลือกสาเหตุย่อย</option>");
+    $(arr).each(function(i) { //to list cities
+    $("#city").append("<option value="+ arr[i].value + ">" + arr[i].display + "</option>")
+    });
+    }
+    });
+
+  function FunctionAutoinput() {
+      var x = document.getElementById("equipcode").value;
+      document.getElementById("equipstrat").value = x;
+  }
+
+
+  function FunctionAutoweather() {
+      var w = document.getElementById("peacode").value;
+      jQuery.ajax({
+        url: "http://127.0.0.1:8000/api/scada/",
+        // url: "https://hookb.in/3OynwLEapdhKeKj2MjmJ",
+        type: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        dataType: 'json',
+        data: JSON.stringify({
+          "peacode" : document.forms["myForm"]["peacode"].value
+
+        })
+
+      })
+      .done(function(data, textStatus, jqXHR) {
+          console.log("HTTP Request Succeeded: " + jqXHR.status);
+          console.log(data); //Return Data
+          if (jqXHR.status == 200) {
+            var des = data['description'];
+
+            if (des=="light rain") {
+              description = "ฝนตกบางเบา"
+            }else if (des=="very heavy rain") {
+              description = "ฝนตกหนัก"
+            }else if(des=="overcast clouds"){
+              description = "เมฆเยอะ"
+            }else if(des=="broken clouds"){
+              description = "เมฆบางส่วน"
+            }
+
+            document.getElementById("weather").value = description;
+
+          };
+        })
+
+
+        return false;
+
+
+  }

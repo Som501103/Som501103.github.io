52f61163a341f3a00e2fa03dabe5aacf75aea833
Som <46430603+Som501103@users.noreply.github.com>
2019-10-21 21:21:17 +0700

    change record data

--- js/script_e1.js
+++ js/script_e1.js
@@ -1,3 +1,29 @@
-let url = location.href;
-let searchParams = new URLSearchParams(url.search);
 console.log(window.location.search);
+var urlParams = new URLSearchParams(window.location.search);
+console.log(urlParams.get('id'));
+var id = urlParams.get('id')
+console.log(id);
+
+jQuery.ajax({
+  url: "https://rc2backend.herokuapp.com/api/getdatae1",
+  // url: "https://hookb.in/3OynwLEapdhKeKj2MjmJ",
+  type: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization":"token 5a5410bf249b5ad186c80a015a8b93abaef18349",
+  },
+  dataType: 'json',
+  data: JSON.stringify({
+    "id" : id
+
+  })
+
+})
+.done(function(data, textStatus, jqXHR) {
+    console.log("HTTP Request Succeeded: " + jqXHR.status);
+    console.log(data); //Return Data
+    if (jqXHR.status == 200) {
+      
+
+    };
+  })

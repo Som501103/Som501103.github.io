91bc1aa68e131802139f84549bccc68da6035fdd
Som <46430603+Som501103@users.noreply.github.com>
2019-11-11 11:34:54 +0700

    Add files via upload

--- js/script_showevent.js
+++ js/script_showevent.js
@@ -24,7 +24,7 @@ function initializeApp() {
       liff.getProfile().then(function (profile) {
           document.getElementById('useridprofilefield').textContent = profile.userId;
           document.getElementById('displaynamefield').textContent = profile.displayName;
-        )}.catch(function (error) {
+        }).catch(function (error) {
             window.alert("Error getting profile: " + error);
         });
     });

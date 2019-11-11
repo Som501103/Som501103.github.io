854b07ceed37679ba0e293fe97bd65992b8e9c1b
Som <46430603+Som501103@users.noreply.github.com>
2019-11-11 15:30:57 +0700

    Add files via upload

--- js/script_showevent.js
+++ js/script_showevent.js
@@ -26,8 +26,50 @@ function initializeApp() {
       liff.getProfile().then(function (profile) {
           document.getElementById('useridprofilefield').textContent = profile.userId;
           alert("uid:"+profile.userId);
+          getstaffid(profile.userId)
           document.getElementById('displaynamefield').textContent = profile.displayName;
+
         }).catch(function (error) {
             window.alert("Error getting profile: " + error);
         });
+
+}
+
+function getstaffid(uid){
+  jQuery.ajax({
+    url: "http://127.0.0.1:8000/api/linegraph/",
+    // url: "https://hookb.in/3OynwLEapdhKeKj2MjmJ",
+    type: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization":"token 5a5410bf249b5ad186c80a015a8b93abaef18349",
+    },
+    dataType: 'json',
+    data: JSON.stringify({
+      "uid" : uid,
+      "datestart" : document.getElementById("datestart").value
+    })
+
+  })
+  .done(function(data, textStatus, jqXHR) {
+      console.log("HTTP Request Succeeded: " + jqXHR.status);
+      console.log(data); //Return Data
+      if (jqXHR.status == 200) {
+        var region = data['region']['label'];
+        var pea = data['showallevent']['label'];
+        var j,y = "";
+        for (j in pea){
+                      y += "<tr><td>"+pea[j][0]+"</td><td>"+pea[j][1]+"</td><td>"+pea[j][2]+"</td>";
+                      // y += "<option value=\""+pea[j][0]+"\">"+pea[j][1]+"</option>";
+                      // console.log(y);
+                    }
+        $('#example').append(y);
+
+        console.log(peacode);
+
+
+      };
+    })
+
+
 }

7419101374cead525a71bab0e270ac208b494b16
Som <46430603+Som501103@users.noreply.github.com>
2019-12-13 21:50:23 +0700

    Add files via upload

--- js/script_e1.js
+++ js/script_e1.js
@@ -49,9 +49,9 @@ jQuery.ajax({
 
 function validateForm(){
   jQuery.ajax({
-    url: "https://e89704f8.ngrok.io/api/getupdate/",
+    // url: "https://e89704f8.ngrok.io/api/getupdate/",
     // url: "http://127.0.0.1:8000/api/getupdate/",
-    // url: "https://rc2backend.herokuapp.com/api/getupdate/",
+    url: "https://rc2backend.herokuapp.com/api/getupdate/",
     type: "POST",
     headers: {
       "Content-Type": "application/json",

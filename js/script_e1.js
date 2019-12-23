9ab37028b6ec97bdfe9a2a3be9a7a93a0fef7a27
Som <46430603+Som501103@users.noreply.github.com>
2019-12-23 10:25:16 +0700

    change url

--- js/script_e1.js
+++ js/script_e1.js
@@ -6,9 +6,9 @@ console.log(id);
 
 
 jQuery.ajax({
-  // url: "https://rc2backend.herokuapp.com/api/getdatae1/",
+  url: "https://rc2backend.herokuapp.com/api/getdatae1/",
   // url: "https://hookb.in/3OynwLEapdhKeKj2MjmJ",
-  url: "https://e89704f8.ngrok.io/api/getdatae1/",
+  // url: "https://e89704f8.ngrok.io/api/getdatae1/",
   type: "POST",
   headers: {
     "Content-Type": "application/json",

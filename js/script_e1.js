2ddc78dfdb744dd7651523cda86122f82fc50f2b
Som <46430603+Som501103@users.noreply.github.com>
2019-12-23 16:43:00 +0700

    image64
    image1

--- js/script_e1.js
+++ js/script_e1.js
@@ -43,6 +43,8 @@ jQuery.ajax({
       // document.getElementById("trip").value = trip;
       console.log(staffid);
       console.log(electrician);
+      console.log(data['image1']);
+
 
     };
   })

0de380b07c0bf9fba099e498fa63cf7cbf76db37
Som <46430603+Som501103@users.noreply.github.com>
2019-12-24 17:55:15 +0700

    Add files via upload

--- js/script_electrician.js
+++ js/script_electrician.js
@@ -286,6 +286,7 @@ function validateForm(){
     dataType: 'json',
     data: JSON.stringify({
       "datehappen" : document.forms["form_e1"]["datehappen"].value,
+      "timehappen" : document.forms["form_e1"]["timehappen"].value,
       "daterestore" : document.forms["form_e1"]["daterestore"].value,
       "timerestore" : document.forms["form_e1"]["timerestore"].value,
       "maincause" : document.forms["form_e1"]["maincause"].value,
